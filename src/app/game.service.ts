4bc147174bbfc95019e75e95923feb587c410a27
Gabriel Luthun <103434050+gabrielluthun@users.noreply.github.com>
2024-06-24 15:16:46 +0200

    feat(game.service.ts) : update damage system

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -45,7 +45,7 @@ export class GameService {
   }
 
   heal() {
-    const healPoints = this.randomNumberService.generate(25, 35);
+    const healPoints = this.randomNumberService.generate(20, 25);
     this.playerHealth = Math.min(this.playerHealth + healPoints, this.playerMaxHealth);
     this.logService.addLog(`Player heals for ${healPoints} health points.`);
     this.monsterAttack();
